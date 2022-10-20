23489b18ea874131f9068f09b019a902672ba34c
jacksonkim0 <112970248+jacksonkim0@users.noreply.github.com>
2022-10-19 22:24:14 -0700

    Add files via upload

---
+++ Python Problem Set #1.py
@@ -0,0 +1,5 @@
+def greeting ():
+    name=input("please enter your name:")
+    print("Oh "+name+" is it? nice to meet you")
+    print("Have a great day!")
+greeting()
---
+++ isMultiple.py
@@ -0,0 +1,6 @@
+def isMultiple(int1,int2):
+    if int1%int2==0:
+        print(str(int1)+" is a multiple of "+str(int2))
+    else:
+        print(str(int1)+" is not a multiple of "+str(int2))
+isMultiple(10,2)
